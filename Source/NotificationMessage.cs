12d92d2ad8ea2f2c87636bcb6d5c7783922553bb
unknown <dmanning23@hotmail.com>
2013-10-17 21:50:41 -0500

    fixed using statements, fixed bugs

--- Source/NotificationMessage.cs
+++ Source/NotificationMessage.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using FontBuddyLib;
 
 namespace ToastBuddyLib
 {
--- Source/ToastBuddy.cs
+++ Source/ToastBuddy.cs
@@ -1,8 +1,8 @@
-using System;
-using System.Collections.Generic;
+using FontBuddyLib;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using FontBuddyLib;
+using System;
+using System.Collections.Generic;
 
 namespace ToastBuddyLib
 {
