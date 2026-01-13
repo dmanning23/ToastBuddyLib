37c3078dcb2317104467c22136889f8775b8628f
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2026-01-12 18:36:14 -0600

    updated package to 5.0

--- ToastBuddyLib/buildNugetPackages.ps1
+++
@@ -1,4 +0,0 @@
-rm *.nupkg
-nuget pack .\ToastBuddyLib.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-cp *.nupkg C:\Projects\Nugets\
-nuget push -source https://www.nuget.org -NonInteractive *.nupkg
